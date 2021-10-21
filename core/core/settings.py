f56f4392b7f9a0fee89b6e714cb5bc0a30988d8e
Boris Bredikhin <boris00043@yandex.ru>
2021-10-21 18:26:01 +0500

    Настроено подключение к БД

--- core/core/settings.py
+++ core/core/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'polznak',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '172.22.0.2',
+        'PORT': 5432,
     }
 }
 
