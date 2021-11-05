755945f4dd8e9214bd1bd56e7b6c01fb2196fd10
Boris Bredikhin <boris00043@yandex.ru>
2021-11-05 13:54:05 +0500

    парсинг

--- parsing/parse.py
+++ parsing/parse.py
@@ -1,9 +1,14 @@
 import typing
-
+from collections import deque
+from typing import Union
+import json
 import requests
 
-access_token = "..." # get it here: https://oauth.vk.com/authorize?client_id=7992889&redirect_uri=localhost&scope=10242&response_type=token
+access_token = "" # get it here: https://oauth.vk.com/authorize?client_id=7992889&redirect_uri=localhost&scope=10242&response_type=token
 
+MAX_NOTES = 1000
+START_USER = None
+FILENAME = "data.json"
 
 class CopyHistory(typing.TypedDict):
     id: int
@@ -38,19 +43,98 @@ class NoteItem(typing.TypedDict):
     short_text_rate: float
     hash: str
 
-class NotesInnerResponse(typing.TypedDict):
+class NotesResponse(typing.TypedDict):
     count: int
     items: list[NoteItem]
 
 
-class NotesResponse(typing.TypedDict):
-    response: NotesInnerResponse
+class RawNotesResponse(typing.TypedDict):
+    response: NotesResponse
+
+class Note(typing.TypedDict):
+    id: int
+    from_id: int
+    owner_id: int
+    date: int
+    post_type: str
+    text: str
+    likes: dict
+
+def get_notes_of_user(user_id: str) -> RawNotesResponse:
+    return requests.get("https://api.vk.com/method/wall.get", {
+        "v": "5.131",
+        'access_token': access_token,
+        'owner_id': user_id,
+    }).json()
+
+def get_notes_with_long_text(
+        notes: RawNotesResponse,
+        minimal_text_length: int = 20
+) -> typing.Iterable[Note]:
+    if 'error' in notes.keys():
+        return
+    for note in notes["response"]["items"]:
+        if len(note["text"]) >= minimal_text_length:
+            yield note
+
+def get_likes(item_id: int) -> list[int]:
+    return requests.get("https://api.vk.com/method/likes.getList", {
+        "v": "5.131",
+        'access_token': access_token,
+        'type': "post",
+        "item_id": item_id
+    }).json()['response']["items"]
 
 
-notes: NotesResponse = requests.get("https://api.vk.com/method/wall.get", {
-    "v": "5.131",
-    'access_token': access_token,
-    'user_id': '191923465',#'150572556'
-}).json()
+user_queue: deque[Union[str, int, None]] = deque()
+user_queue.append(START_USER)
+user_queue.append(8423123)
+user_queue.append(146737204)
+user_queue.append(5152425)
+user_queue.append(150572556)
+user_queue.append(208660858)
+user_queue.append(481752039)
+user_queue.append(13459300)
+user_queue.append(3945237)
+user_queue.append(338031983)
+visited_users: set[str] = set()
 
-print(notes)
+parsed_notes: list = []
+
+while len(user_queue)>0 and len(parsed_notes) < MAX_NOTES:
+    current_user = user_queue.popleft()
+
+    if current_user in visited_users:
+        continue
+    visited_users.add(current_user)
+
+    notes: RawNotesResponse = get_notes_of_user(current_user)
+
+    notes_queue: deque[Note] = deque(get_notes_with_long_text(notes))
+
+
+    while len(notes_queue) > 0 and len(parsed_notes) < MAX_NOTES:
+        current_note = notes_queue.popleft()
+        likes = get_likes(current_note['id'])
+        if len(likes)==0:
+            continue
+        user_queue.extend(likes)
+        parsed_notes.append({
+            'id': current_note['id'],
+            'from_id': current_note['from_id'],
+            'owner_id': current_note['owner_id'],
+            'date': current_note['date'],
+            'text': current_note['text'],
+            'liked_by': likes
+        })
+    print(len(parsed_notes),MAX_NOTES, sep='/')
+
+with open(FILENAME, 'w') as f:
+    f.write(json.dumps(
+        {
+            "users": list(visited_users),
+            "notes": parsed_notes
+        },
+        indent=4,
+        ensure_ascii=False
+    ))
